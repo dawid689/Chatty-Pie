623de0c6fae9b13a5bca732daedc343d7bdeb3bf
Dawid <dawid975310@gmail.com>
2022-11-16 20:47:38 +0100

    Login and signup pages

---
+++ app/src/main/java/com/example/chattypie/LogIn.kt
@@ -0,0 +1,31 @@
+package com.example.chattypie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LogIn : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogIn: Button
+    private lateinit var btnSignUp: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogIn = findViewById(R.id.btnLogIn)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
