f88f364ac8c11eb7c5caa3c786dce2a49ab14a26
Dawid <dawid975310@gmail.com>
2022-11-19 19:49:40 +0100

    Logic for login and signup, user model

--- app/src/main/java/com/example/chattypie/LogIn.kt
+++ app/src/main/java/com/example/chattypie/LogIn.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class LogIn : AppCompatActivity() {
 
@@ -13,9 +16,15 @@ class LogIn : AppCompatActivity() {
     private lateinit var btnLogIn: Button
     private lateinit var btnSignUp: Button
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -27,5 +36,25 @@ class LogIn : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnLogIn.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password);
+        }
+
+
+    }
+
+    private fun login(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@LogIn, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@LogIn, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
